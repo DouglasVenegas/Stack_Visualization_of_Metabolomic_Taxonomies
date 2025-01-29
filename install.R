db96082e7592ec2b32695adea5dd03bd34fc2414
Douglas Venegas <douglas.venegas@ucr.ac.cr>
2025-01-29 16:34:18 -0600

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-install.packages(c("readr","fuzzyjoin", "tidyverse", "readxl", "openxlsx", "dplyr","stringr", "tidyr","ggplot2", "writex1")
+install.packages(c("readr","fuzzyjoin", "tidyverse", "readxl", "openxlsx", "dplyr","stringr", "tidyr","ggplot2", "writex1"))
